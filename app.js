af4d613e1fba8bdc889895b819946763eb9a67d1
Dawid Kurkiewicz <dawid.kurkiewicz@billennium.com>
2019-07-19 15:49:21 +0200

    fixed height on phones

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const canvas = document.querySelector("#draw")
 const ctx = canvas.getContext("2d")
 canvas.width = window.innerWidth
-canvas.height = window.innerHeight
+canvas.height = window.innerHeight -500
 ctx.strokeStyle = document.querySelector("#base").value
 ctx.lineJoin = "round"
 ctx.lineCap = "round"
