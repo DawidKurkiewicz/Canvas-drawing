46387a1bbf3ff0da498f1629ca12cf8bf29bce06
Dawid Kurkiewicz <dawid.kurkiewicz@billennium.com>
2019-07-19 14:47:33 +0200

    added few options

--- app.js
+++ app.js
@@ -2,22 +2,27 @@ const canvas = document.querySelector("#draw")
 const ctx = canvas.getContext("2d")
 canvas.width = window.innerWidth
 canvas.height = window.innerHeight
-ctx.strokeStyle = "#BADA55"
+ctx.strokeStyle = document.querySelector("#base").value
 ctx.lineJoin = "round"
 ctx.lineCap = "round"
-ctx.lineWidth=30
-
+ctx.lineWidth= 30
+btn=document.querySelector("#hue")
 let isDrawing = false;
 let lastX = 0
 let lastY = 0
+
 function draw(e) {
     if (!isDrawing)return
     console.log(e)
+    ctx.strokeStyle = document.querySelector("#base").value 
+    ctx.lineWidth= document.querySelector("#number").value
+
     ctx.beginPath();
     ctx.moveTo(lastX, lastY)
     ctx.lineTo(e.offsetX, e.offsetY)
     ctx.stroke();
     [lastX,lastY]= [e.offsetX, e.offsetY]
+
 }
 canvas.addEventListener("mousedown", (e) => {
     isDrawing = true;
